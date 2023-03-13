5b872ce74eec0f07366e4035aa34d4a808127fd1
Huy Hoan <hoanndhpd08255@fpt.edu.vn>
2023-03-13 13:37:33 +0700

    upload

---
+++ lab1/lab1.js
@@ -0,0 +1,8 @@
+let a = prompt("nhap so nguen A: ");
+let a1=parseInt(a);
+let b= prompt("nhap so nguen B: ");
+let b1 = parseInt(b);
+
+let c = parseInt(a)+ parseInt(b);
+document.write(c);
+console.log(c);
