312e3106953bc258038e8df11f6bd7ef20e70081
Huy Hoan <hoanndhpd08255@fpt.edu.vn>
2023-04-07 12:22:53 +0700

    upp

--- lab7/js.js
+++ lab7/js.js
@@ -1,15 +1,14 @@
 function allowDrop(ev) {
     ev.preventDefault(); 
 }
-
 //drag
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
-
 //drog
 function drop(ev) {
     ev.preventDefault(); 
     var data = ev.dataTransfer.getData("text"); //lấy dữ liệu được kéo
-    ev.target.appendChild(document.getElementById(data)); //hàm appendchill() dùng để thêm phần tử đó vào trong phần tử mục tiêu được thả vào
+    ev.target.appendChild(document.getElementById(data)); 
+    //hàm appendchill() dùng để thêm phần tử đó vào trong phần tử mục tiêu được thả vào
 }
